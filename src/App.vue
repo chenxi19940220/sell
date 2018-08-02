9614e64cc7ba7c70e99d41f2dd0002683d529e93
cx <229168514@qq.com>
2018-08-02 23:28:40 +0800

    初始化项目 header组件开发

---
+++ src/App.vue
@@ -0,0 +1,70 @@
+<template>
+  <div id="app">
+    <m-header :seller="seller"></m-header>
+    <div class="tab border-bottom">
+      <router-link tag="div" class="tab-item" :to="{path:'/goods'}">
+        <span class="tab-link">商品</span>
+      </router-link>
+      <router-link tag="div" class="tab-item" :to="{path:'/ratings'}">
+        <span class="tab-link">评论</span>
+      </router-link>
+      <router-link tag="div" class="tab-item" :to="{path:'/seller'}">
+        <span class="tab-link">商家</span>
+      </router-link>
+    </div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+import axios from 'axios'
+import MHeader from 'components/m-header/m-header'
+
+export default {
+  name: 'App',
+  components: {
+    MHeader
+  },
+  data () {
+    return {
+      seller: {}
+    }
+  },
+  created () {
+    this._getSeller()
+  },
+  methods: {
+    _getSeller () {
+      axios.get('https://www.easy-mock.com/mock/5b61b78f2205a5414ac526b7/sell/data')
+        .then((res) => {
+          console.log(res)
+          if (res.request.status === 200 && res.request.readyState === 4) {
+            console.log(res.data)
+            this.seller = res.data.seller
+          }
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+    }
+  }
+}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+.tab
+  display flex
+  width 100%
+  height 40px
+  line-height 40px
+  .tab-item
+    flex 1
+    text-align center
+    & > span
+      display block
+      font-size 14px
+      color rgb(77, 85, 93)
+  .active
+    & > span
+      color rgb(240, 20, 20)
+</style>
---
+++ src/components/goods/goods.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="goods">
+    i am goods
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+  name: 'Goods'
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
